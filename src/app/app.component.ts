2f468b2c7ffeed3e02efddee10693c65e10a32b7
rojo7006 <jose.rosado.consultant@nielsen.com>
2019-07-08 00:11:08 -0500

    Add project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'lorval2019';
+  config: any;
+  fullpage_api: any;
+
+  constructor() {
+    // for more details on config options please visit fullPage.js docs
+    this.config = {
+      // fullpage options
+      licenseKey: 'F0C98BD8-1E5C4649-89CD41BD-6DB81FC1',
+      anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+      menu: '#menu',
+      // fullpage callbacks
+      afterResize: () => {
+        console.log("After resize");
+      },
+      afterLoad: (origin, destination, direction) => {
+        if(origin){
+          console.log(origin.index);
+        }
+      }
+    };
+  }
+
+  getRef(fullPageRef) {
+    this.fullpage_api = fullPageRef;
+  }
 }
